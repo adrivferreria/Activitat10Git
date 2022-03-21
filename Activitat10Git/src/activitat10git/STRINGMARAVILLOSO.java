8f0aa1daccdd3f046004e6b9fe3ce59fda62b7be
adrivferreria <>
2022-03-21 17:28:22 +0100

    He pasado los prints a metodos

--- Activitat10Git/src/activitat10git/STRINGMARAVILLOSO.java
+++ Activitat10Git/src/activitat10git/STRINGMARAVILLOSO.java
@@ -4,47 +4,59 @@
  * and open the template in the editor.
  */
 package activitat10git;
-public class STRINGMARAVILLOSO{
-    public static void main(String[] args){
-        String M = "MARAVILLOSO";
-        
-        System.out.println("LA versió que tenim a github ha estat actualitzada i és mes actual que la que tinc en local");
- 
-        //Imprime los primeros dos triangulos de izq a derecha de la primera parte (parte superios)
-        for (int i =0; i<M.length();i++){
+
+public class STRINGMARAVILLOSO {
+
+    public static void parteSuperior(String M) {
+        //Imprime los primeros dos triangulos de izq a derecha de la primera parte (parte superior)
+        for (int i = 0; i < M.length(); i++) {
             //Imprime espacios de la primera parte para el primer triangulo 
-            for (int j=0; j < i  ;++j){
+            for (int j = 0; j < i; ++j) {
                 System.out.print(" ");
             }
             //Imprime el primer triangulo de la primer parte 
-            for (int j=i; j < M.length() ;++j){
+            for (int j = i; j < M.length(); ++j) {
                 System.out.print(M.charAt(j));
             }
             //Imprime espacios entre el primero y segundo triangulo de la primera parte
-            for (int j=i; j < M.length() ;++j){
+            for (int j = i; j < M.length(); ++j) {
                 System.out.print(" ");
             }
             //Imprime el segundo triangulo de la primera parte
-            for (int j= M.length() - i - 1; j < M.length(); ++j){
+            for (int j = M.length() - i - 1; j < M.length(); ++j) {
                 System.out.print(M.charAt(j));
             }
             System.out.println();
         }
+    }
+
+    public static void parteInferior(String M) {
         //Imprime los segundos dos triangulos de izq a derecha de la segunda parte (parte inferior)
-        for (int i =0; i<M.length();i++){
+        for (int i = 0; i < M.length(); i++) {
             //Imprime primer triangulo de la segunda parte
-            for (int j= i; j<M.length(); ++j){
+            for (int j = i; j < M.length(); ++j) {
                 System.out.print(M.charAt(j));
             }
             //Imprime espacios entre primer y segundo triangulo
-            for (int j=0; j <= i ;++j){
+            for (int j = 0; j <= i; ++j) {
                 System.out.print(" ");
             }
             //Imprime segundo triangulo de la segunda parte  
-            for(int j=0; j < i + 1; ++j){
+            for (int j = 0; j < i + 1; ++j) {
                 System.out.print(M.charAt(j));
             }
             System.out.println();
         }
-    }    
+    }
+
+    public static void main(String[] args) {
+        String M = "MARAVILLOSO";
+
+        System.out.println("LA versió que tenim a github ha estat actualitzada i és mes actual que la que tinc en local");
+
+        parteSuperior(M);
+
+        parteInferior(M);
+
+    }
 }
